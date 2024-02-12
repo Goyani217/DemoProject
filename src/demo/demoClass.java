bf7fd36cf029bbd14bbbeb949933d19ca8005615
Jay Goyani <jayk.goyani@gmail.com>
2024-02-12 12:44:51 +0530

    first commit

---
+++ src/demo/demoClass.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class demoClass {
+	
+	public static void show() {
+		// TODO Auto-generated method stub
+		System.out.println("in show method");
+	}
+}
+
+class demo2{
+	public static void main(String[] args) {
+		demoClass.show();
+	}
+}
